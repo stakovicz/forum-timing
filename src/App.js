45beff43748e6530e5fc39045984fce280d4c8b6
Albin Kester <83301974+stakovicz@users.noreply.github.com>
2025-09-26 23:33:25 +0200

    dynamic fetch (#1)

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import 'moment/locale/fr';
 
 import './App.css';
 import {useEffect, useState} from "react";
-//import timings from "./timings"
-import timings from "./timings-tmv"
+//import tests from "./timings"
+//import timings from "./timings-tmv"
+//import afup from "./afup"
+import logo from "./forum.svg"
 
 function Ftn({number, unit, hideIfZero = true}) {
     if (hideIfZero && !number) {
@@ -16,6 +18,22 @@ function Ftn({number, unit, hideIfZero = true}) {
     </span>
 }
 
+function SelectTrack({tracks, setTrack})  {
+    return (
+        <div className="select-track">
+            <img src={logo} alt="" className="logo"/>
+            <div>
+                <select onChange={(e) => setTrack(e.target.value)}>
+                    <option>Choisissez une track</option>
+                    {tracks.map((track, i) => (
+                        <option key={i} value={track}>{track}</option>
+                    ))}
+                </select>
+            </div>
+        </div>
+    )
+}
+
 function Upcoming({current, timings}) {
 
     const currentIndex = timings.indexOf(current);
@@ -25,6 +43,9 @@ function Upcoming({current, timings}) {
         {timings.map((timing, i) => {
             const duration = timing.to ? moment.duration(moment(timing.to).diff(timing.from)).minutes() : null;
             const passed = i < nextIndex;
+            if (passed) {
+                return null;
+            }
 
             return (<div key={i} className={`${passed ? "passed" : ""}`}>
                 <div className="datetime">
@@ -40,11 +61,17 @@ function Upcoming({current, timings}) {
 
 function Current({now, timing}) {
     const [rest, setRest] = useState(moment.duration());
+    const [inTalk, setInTalk] = useState(false)
     useEffect(() => {
         if (!timing) {
             return;
         }
-        setRest(moment.duration(moment(timing.from).diff()));
+
+        setRest(moment.duration(moment(timing.to ?? timing.from).diff()));
+        if (timing.to) {
+            setInTalk(moment().isBetween(timing.from, timing.to));
+        }
+
     }, [now, timing]);
 
     if (!timing) {
@@ -60,6 +87,8 @@ function Current({now, timing}) {
 
     return <div className="App-next">
         <div className="rest" style={{color: color}}>
+            {inTalk && "🗣️ "}
+            <Ftn number={rest.days()} unit="j" />
             <Ftn number={rest.hours()} unit="h" />
             <Ftn number={rest.minutes()} unit="min" />
             <Ftn number={rest.seconds()} unit="s" hideIfZero={false} />
@@ -73,22 +102,65 @@ function Current({now, timing}) {
     </div>
 }
 
-
 function App() {
     const [now, setNow] = useState(moment());
-    const [current, setCurrent] = useState()
+    const [current, setCurrent] = useState();
+    const [timings, setTimings] = useState([]);
+    const [tracks, setTracks] = useState([]);
+    const [track, setTrack] = useState(undefined);
+
+    useEffect(() => {
+        fetch("https://afup.org/event/forumphp2025/openfeedback.json")
+            .then((response) => {
+                response.json()
+                    .then((data) => {
+                        console.log(data)
+                    const timingsFromSessions = Object.values(data)
+                        .filter((session) => {
+                            return session.trackTitle === track;
+                        })
+                        .sort((a, b) => moment(a.startTime) > moment(b.startTime))
+                        .map((session) => {
+                            return {
+                                name: session.title,
+                                from: session.startTime,
+                                to: session.endTime,
+                            }
+                        });
+
+                    setTimings(timingsFromSessions);
+                    //setTimings(tests);
+
+                    const trackFromSessions = Object.values(data)
+                        .map((session) => {
+                            return session.trackTitle;
+                        })
+                        .filter((value, index, array) => {
+                            return array.indexOf(value) === index
+                        })
+                        .sort((a, b) => {
+                            const [, roomA] = a.split(" - ");
+                            const [, roomB] = b.split(" - ");
+
+                            return roomA.localeCompare(roomB);
+                        });
+                    setTracks(trackFromSessions);
+                })
+            })
+    }, [track]);
 
     useEffect(() => {
         const interval = setInterval(() => {
             setNow(moment());
 
             setCurrent(timings.find((timing) => {
-                return moment(timing.from).diff(now) >= 0;
+                return moment(timing.from).diff(now) >= 0 ||
+                    (timing.to && moment(timing.to).diff(now) >= 0);
             }));
         }, 1000);
 
         return () => {clearInterval(interval)}
-    }, [now]);
+    }, [now, timings]);
 
     return (
         <div className="App">
@@ -97,6 +169,7 @@ function App() {
                     <div className="date">
                         {now.format("ddd DD MMM YYYY")}
                     </div>
+
                     <div className="time">
                         {now.format("HH:mm:ss")}
                     </div>
@@ -105,6 +178,7 @@ function App() {
             <main className="App-main">
                 <Current now={now} timing={current} />
                 <Upcoming current={current} timings={timings} />
+                <SelectTrack tracks={tracks} setTrack={setTrack} />
             </main>
         </div>
     );
--- src/timings.js
+++ src/timings.js
@@ -7,7 +7,8 @@ const timings = [
     },
     {
         name: "Départ Trail Long",
-        from: "2025-10-05T08:00:00.000+02:00",
+        from: "2025-09-26T22:00:00.000+02:00",
+        to: "2025-09-26T23:30:00.000+02:00",
     },
     {
         name: "Départ Trail Long",
